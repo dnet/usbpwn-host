2df20374e9255063ac9f2a23ab253bd48d93768d
András Veres-Szentkirályi <vsza@vsza.hu>
2012-10-15 15:14:07 +0200

    removed unused method head check_state()
    implementation was removed in 2a61b27ad08b2a6b0a50a8c915cb784f10cc47a2

--- win32kbd.h
+++ win32kbd.h
@@ -37,7 +37,6 @@
 #define CAPS VK_CAPITAL
 
 void toggle_key(const BYTE key);
-uint8_t check_state(const BYTE key);
 void set_lock(const BYTE key, const uint8_t state);
 
 #endif /* _WIN32_KBD_H_ */
